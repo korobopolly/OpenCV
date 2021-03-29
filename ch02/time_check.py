06c62b7dd50cedc3e026af19eafc62cff19a4aa6
korobopolly <72500196+korobopolly@users.noreply.github.com>
2021-03-29 13:51:22 +0900

    연산시간 측정방법

--- ch02/time_check.py
+++ ch02/time_check.py
@@ -4,17 +4,23 @@ import numpy as np
 import cv2
 
 
-img = cv2.imread('hongkong.jpg')
+img = cv2.imread('ch02\\hongkong.jpg')
 
 tm = cv2.TickMeter()
 
 tm.reset()
+
 tm.start()
 t1 = time.time()
 
 edge = cv2.Canny(img, 50, 150)
 
 tm.stop()
-print('time:', (time.time() - t1) * 1000)
-print('Elapsed time: {}ms.'.format(tm.getTimeMilli()))
+
+print('time:', (time.time() - t1) * 1000) #파이썬 방식
+print('Elapsed time: {}ms.'.format(tm.getTimeMilli())) #Open CV 방식
+cv2.imshow('image',edge)
+cv2.waitKey()
+
+cv2.destroyAllWindows()
 

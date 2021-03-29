002c2a9f9095e203dfe93ac88b51372059f8c1d4
korobopolly <72500196+korobopolly@users.noreply.github.com>
2021-03-29 12:50:10 +0900

    키보드 이벤트 처리하기

--- ch02/keyboard.py
+++ ch02/keyboard.py
@@ -3,7 +3,7 @@ import numpy as np
 import cv2
 
 
-img = cv2.imread('cat.bmp', cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('ch02\\cat.bmp', cv2.IMREAD_GRAYSCALE)
 
 if img is None:
     print('Image load failed!')
@@ -13,11 +13,11 @@ cv2.namedWindow('image')
 cv2.imshow('image', img)
 
 while True:
-    keycode = cv2.waitKey()
-    if keycode == ord('i') or keycode == ord('I'):
-        img = ~img
-        cv2.imshow('image', img)
-    elif keycode == 27:
+    key=cv2.waitKey() #키 값을 한번만 기다림 (중요도 높음)
+    if key==27:
         break
+    elif key==ord('i')or key==ord('I'):
+        img=~img
+        cv2.imshow('~img',~img)
 
 cv2.destroyAllWindows()

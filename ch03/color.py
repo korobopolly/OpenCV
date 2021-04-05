521d2dfd8f87d28b634b5fd67484b62cf1819d42
korobopolly <72500196+korobopolly@users.noreply.github.com>
2021-04-05 12:26:44 +0900

    컬러 영상 처리와 색 공간

--- ch03/color.py
+++ ch03/color.py
@@ -4,7 +4,7 @@ import cv2
 
 
 # 컬러 영상 불러오기
-src = cv2.imread('candies.png', cv2.IMREAD_COLOR)
+src = cv2.imread('ch03//candies.png', cv2.IMREAD_COLOR)
 
 if src is None:
     print('Image load failed!')
@@ -15,16 +15,17 @@ print('src.shape:', src.shape)  # src.shape: (480, 640, 3)
 print('src.dtype:', src.dtype)  # src.dtype: uint8
 
 # RGB 색 평면 분할
-b_plane, g_plane, r_plane = cv2.split(src)
+src_hsv=cv2.cvtColor(src,cv2.COLOR_BGR2YCrCb)
+planes = cv2.split(src_hsv)
 
 #b_plane = src[:, :, 0]
 #g_plane = src[:, :, 1]
 #r_plane = src[:, :, 2]
 
-cv2.imshow('src', src)
-cv2.imshow('B_plane', b_plane)
-cv2.imshow('G_plane', g_plane)
-cv2.imshow('R_plane', r_plane)
+cv2.imshow('src', src_hsv)
+cv2.imshow('B_plane', planes[0])
+cv2.imshow('G_plane', planes[1])
+cv2.imshow('R_plane', planes[2])
 cv2.waitKey()
 
 cv2.destroyAllWindows()

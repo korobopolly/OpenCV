82f94f8dceb189dbf968436514423d15a6767380
korobopolly <72500196+korobopolly@users.noreply.github.com>
2021-04-12 13:54:24 +0900

    크로마키 영상 합성

--- ch03/chroma_key.py
+++ ch03/chroma_key.py
@@ -4,27 +4,37 @@ import cv2
 
 
 # 녹색 배경 동영상
-cap1 = cv2.VideoCapture('woman.mp4')
+cap1 = cv2.VideoCapture(0) #'ch03//woman.mp4'
 
 if not cap1.isOpened():
     print('video open failed!')
     sys.exit()
 
 # 비오는 배경 동영상
-cap2 = cv2.VideoCapture('raining.mp4')
+cap2 = cv2.VideoCapture('ch03//videoplayback.webm') #'ch03//raining.mp4'
 
 if not cap2.isOpened():
     print('video open failed!')
     sys.exit()
 
 # 두 동영상의 크기, FPS는 같다고 가정
+w=round(cap1.get(cv2.CAP_PROP_FRAME_WIDTH))
+h=round(cap1.get(cv2.CAP_PROP_FRAME_HEIGHT))
+w1=round(cap2.get(cv2.CAP_PROP_FRAME_WIDTH))
+h1=round(cap2.get(cv2.CAP_PROP_FRAME_HEIGHT))
 frame_cnt1 = round(cap1.get(cv2.CAP_PROP_FRAME_COUNT))
 frame_cnt2 = round(cap2.get(cv2.CAP_PROP_FRAME_COUNT))
+print('w x h: {} x {}'.format(w,h))
+print('w1 x h1: {} x {}'.format(w1,h1))
 print('frame_cnt1:', frame_cnt1)
 print('frame_cnt2:', frame_cnt2)
 
 fps = cap1.get(cv2.CAP_PROP_FPS)
 delay = int(1000 / fps)
+
+#출력 동영상 객체 생성
+fourcc=cv2.VideoWriter_fourcc(*'DIVX')
+out=cv2.VideoWriter('output1.avi',fourcc,fps,(w,h))
 
 # 합성 여부 플래그
 do_composit = False
@@ -43,10 +53,18 @@ while True:
         if not ret2:
             break
 
+        frame2=cv2.resize(frame2,(w,h)) #영상 크기 조절 중요!
+
         # HSV 색 공간에서 녹색 영역을 검출하여 합성
         hsv = cv2.cvtColor(frame1, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(hsv, (50, 150, 0), (70, 255, 255))
+        mask = cv2.inRange(hsv, (10, 0, 0), (100, 255, 255))
         cv2.copyTo(frame2, mask, frame1)
+        cv2.imshow('mask',mask)
+
+
+    out.write(frame1) #영상 쓰기
+
+    
 
     cv2.imshow('frame', frame1)
     key = cv2.waitKey(delay)
@@ -59,4 +77,5 @@ while True:
 
 cap1.release()
 cap2.release()
+out.release()
 cv2.destroyAllWindows()
